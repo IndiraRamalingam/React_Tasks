a328ccb4aa1b3d9d0cfd46589fb0fc719b575da9
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-06-15 16:36:52 +0530

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,78 +1,112 @@
-//card_1
-function Card1()
-{
+import React from "react";
+//Card 1
+function Card1(){
   return(
-    <div>
-      <h5>FREE</h5>
-      <h1>$0/month</h1>
-      <ul>
-        <li>Single User</li>
-        <li>5GB Storage</li>
-        <li>Unlimited Public Projects</li>
-        <li>Community Access</li>
-        <li>Unlimited Private Projects</li>
-        <li>Dedicated Phone Support</li>
-        <li>Free Subdomain</li>
-        <li>Monthly Status Reports</li>
-      </ul>
-      <button>BUTTON</button>
-    </div>
+    <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+            
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                Private Projects</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                Phone Support</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+              </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
-//Card_2
-function Card2()
-{
+//Card 2
+function Card2(){
   return(
-    <div>
-      <h5>PLUS</h5>
-      <h1>$9/month</h1>
-      <ul>
-        <li><b>5 USers</b></li>
-        <li>50GB Storage</li>
-        <li>Unlimited Public Projects</li>
-        <li>Community Access</li>
-        <li>Unlimited Private Projects</li>
-        <li>Dedicated Phone Support</li>
-        <li>Free Subdomain</li>
-        <li>Monthly Status Reports</li>
-      </ul>
-      <button>BUTTON</button>
-    </div>
+    <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
+            <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+            
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
-//Card_3
-function Card3()
-{
+//Card 3
+function Card3(){
   return(
-    <div>
-      <h5>PRO</h5>
-      <h1>$49/month</h1>
-      <ul>
-        <li><b>Unlimited Users</b></li>
-        <li>5GB Storage</li>
-        <li>Unlimited Public Projects</li>
-        <li>Community Access</li>
-        <li>Unlimited Private Projects</li>
-        <li>Dedicated Phone Support</li>
-        <li><b>Unlimited </b>Free Subdomains</li>
-        <li>Monthly Status Reports</li>
-      </ul>
-      <button>BUTTON</button>
+    <div class="col-lg-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
+        <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+        
+        <ul class="fa-ul">
+          <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong>
+          </li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free
+            Subdomains</li>
+          <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+        </ul>
+        <div class="d-grid">
+          <a href="#" class="btn btn-primary text-uppercase">Button</a>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
-
-function App()
-{
+function App() {
   return (
-    <div>   
-      <Card1 />
-      <Card2 />
-      <Card3 />
+    <>
+    <section class="pricing py-5">
+    <div class="container">
+      <div class="row">
+        <Card1 />
+        <Card2 />
+        <Card3 />
+      </div>
     </div>
-  )
+    </section>
+    </>
+  );
 }
+
 export default App;
+
+
+
